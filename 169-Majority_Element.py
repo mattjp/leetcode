fabaf9311d84282c0cb6ce89e22f6a2cfb841be2
mattjp <mattjp@umich.edu>
2019-05-09 19:45:24 -0400

    i can only get easy ones today :(

---
+++ 169-Majority_Element.py
@@ -0,0 +1,16 @@
+# 169. Majority Element
+#
+# Given an array of size n, find the majority element. The majority element is 
+# the element that appears more than floor(n/2) times.
+
+class Solution:
+  def majorityElement(self, nums: List[int]) -> int:
+    n = len(nums)
+    d = {}
+    for num in nums:
+      if num not in d:
+        d[num] = 1
+      else:
+        d[num] += 1
+      if d[num] > (n/2):
+        return num
