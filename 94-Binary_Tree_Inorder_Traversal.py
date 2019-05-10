2859b6eaa4c275e706ef8b052bf42c6d5767d66d
mattjp <mattjp@umich.edu>
2019-05-09 20:32:25 -0400

    recursion

---
+++ 94-Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,16 @@
+# 94. Binary Tree Inorder Traversal
+# 
+# Given a binary tree, return the inorder traversal of its nodes' values.
+
+class Solution:
+  def helper(self, root, x):
+    if root is None:
+      return x
+    self.helper(root.left, x)
+    x.append(root.val)
+    self.helper(root.right, x)
+    return x
+
+  def inorderTraversal(self, root: TreeNode) -> List[int]:
+    x = []
+    return self.helper(root, x)
