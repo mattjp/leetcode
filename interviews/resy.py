cdfffd9a0acb7ee0e871abe29f34066d66fdf70e
mattjp <mattjp@umich.edu>
2019-06-01 13:13:16 -0400

    added resy so i don't forget

---
+++ interviews/resy.py
@@ -0,0 +1,6 @@
+# Given a string and a list of words, find the shortest substring such that the
+# substring contains all words present in the list.
+
+class Solution:
+  def find_substring(self, text, words):
+    print("What now")
