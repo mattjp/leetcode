5dba531f2c2b2c2b5bfa5b66609092cf7ff31cf3
mattjp <mattjp@umich.edu>
2019-06-09 20:01:46 -0400

    patek water

---
+++ practice/011-Container_With_Most_Water.py
@@ -0,0 +1,22 @@
+# 11. Container With Most Water
+#
+# Given n non-negative integers a1, a2, ..., an , where each represents a point
+# at coordinate (i, ai). n vertical lines are drawn such that the two endpoints
+# of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis
+# forms a container, such that the container contains the most water.
+#
+# Note: You may not slant the container and n is at least 2.
+
+class Solution:
+  def calcArea(self, h, w):
+    return h*w
+
+  def maxArea(self, height: List[int]) -> int:
+    res, l, r = 0, 0, len(height)-1
+    while l < r:
+      res = max(res, self.calcArea(min(height[l], height[r]), r-l))
+      if height[l] < height[r]:
+        l += 1
+      else:
+        r -= 1
+    return res
