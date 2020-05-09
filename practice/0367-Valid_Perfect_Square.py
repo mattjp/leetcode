86bb490d8d26fc3068e9ab16ce6b717ea43ac4f3
Matt Peretick <mattjp@umich.edu>
2020-05-09 09:20:28 -0400

    Update 0367-Valid_Perfect_Square.py

--- practice/0367-Valid_Perfect_Square.py
+++ practice/0367-Valid_Perfect_Square.py
@@ -7,3 +7,20 @@ class Solution:
       if i*i > num:
         return False
     return False
+  
+  # solved using binary search (smarter)
+  def isPerfectSquare(self, num: int) -> bool:
+    def binarySearch(num):
+      start = 0
+      end = num
+      while start < end:
+        mid = start + ((end - start) // 2)
+        res = mid * mid
+        if res == num:
+          return True
+        elif res < num:
+          start = mid+1
+        else:
+          end = mid-1
+      return False
+    return binarySearch(num)
