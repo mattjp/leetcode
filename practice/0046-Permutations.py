ebd1dc2245111fb5ba6d639d72cc1ed5eba1ca50
Matt Peretick <mattjp@umich.edu>
2020-05-16 08:40:21 -0400

    Update 0046-Permutations.py

--- practice/0046-Permutations.py
+++ practice/0046-Permutations.py
@@ -13,3 +13,22 @@ class Solution(object):
     for permutation in permutations(nums):
       res.append(permutation)
     return res
+  
+# solved again without using cheating libraries
+class Solution:
+  def permute(self, nums: List[int]) -> List[List[int]]:
+    self.result = []
+
+    def loop(available: List[int], current: List[int]) -> List[List[int]]:
+        if len(available) == 0:
+          self.result.append(current.copy()) # use shallow copy so it's not a bunch of refs
+          return
+        for i in range(len(available)):
+          a = available.pop(i) 
+          current.append(a)
+          loop(available, current)
+          current.pop()
+          available.insert(i,a) # put `a` back where it came from (don't mess up loop)
+
+    loop(nums, [])
+    return self.result
