232721c6fac447efe4a1983197486f79430c3c68
Matt Peretick <mattjp@umich.edu>
2020-05-22 09:45:38 -0400

    Create 0075-Sort_Colors.py

---
+++ practice/medium/0075-Sort_Colors.py
@@ -0,0 +1,23 @@
+class Solution:
+  def sortColors(self, nums: List[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    # if 0 inserte at 0s pointer (iterate 1s pointer)
+    # if 1, pop and insert at 1s pointer (end of ones)
+    # if 2, pop and insert at end
+
+    zeros = len(nums)-1
+    ones = len(nums)-1
+
+    for _ in range(len(nums)):
+      cur = nums.pop(0)
+      if cur == 0:
+        nums.insert(zeros, cur)
+      else:
+        if cur == 1:
+          nums.insert(ones, cur)
+        else:
+          nums.append(cur)
+        ones -= 1
+      zeros -= 1
