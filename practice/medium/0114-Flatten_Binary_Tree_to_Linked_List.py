6c90fc8f7791eb8eb6809317be7a6db0f3195a85
Matt Peretick <mattjp@umich.edu>
2020-05-26 17:32:55 -0400

    Update 0114-Flatten_Binary_Tree_to_Linked_List.py

--- practice/medium/0114-Flatten_Binary_Tree_to_Linked_List.py
+++ practice/medium/0114-Flatten_Binary_Tree_to_Linked_List.py
@@ -24,3 +24,31 @@ class Solution:
       itr = itr.right
 
     return root
+  
+  # solved again using recursion
+class Solution:
+  def flatten(self, root: TreeNode) -> None:
+  """
+  Do not return anything, modify root in-place instead.
+  """
+
+  def loop(root: TreeNode) -> TreeNode:
+    if root == None:
+      return None
+    if root.right == None and root.left == None:
+      return root
+
+    l_tail = loop(root.left) # 
+    r_tail = loop(root.right)
+
+    # reorder the current subtree
+    if l_tail != None:
+      l_tail.right = root.right # left tail must point to current left
+      root.right = root.left # swap right in for left
+      root.left = None
+
+    # return the deepest node coming from the left subtree
+    return r_tail if r_tail != None else l_tail
+
+  # do work
+  loop(root)
