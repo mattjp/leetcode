de4488063eb65e516e81d99e3aba75f559d124a4
Matt Peretick <mattjp@umich.edu>
2020-05-26 20:00:51 -0400

    Update and rename practice/0079-Word_Search.py to practice/medium/0079-Word_Search.py

--- practice/medium/0079-Word_Search.py
+++ practice/medium/0079-Word_Search.py
@@ -37,3 +37,40 @@ class Solution:
           if found:
             return True
     return False
+  
+  
+# solved again
+class Solution:
+  def exist(self, board: List[List[str]], word: str) -> bool:
+
+    self.max_i = len(board)
+    self.max_j = len(board[0])
+
+    target = word[0]
+
+    def exists(board, word, visited, current, i, j, k) -> bool:
+      if current == word:
+        return True
+
+      # handle all false cases
+      if i < 0 or i >= self.max_i or j < 0 or j >= self.max_j or board[i][j] != word[k] or (i,j) in visited:
+        return False
+
+      tmp = current + board[i][j]
+
+      visited.add((i,j))
+
+      # terminate early, don't check if already found
+      found = exists(board, word, visited, tmp, i-1, j, k+1) \
+      or exists(board, word, visited, tmp, i+1, j, k+1) \
+      or exists(board, word, visited, tmp, i, j-1, k+1) \
+      or exists(board, word, visited, tmp, i, j+1, k+1)
+
+      visited.remove((i,j))
+      return found
+
+    for i in range(self.max_i):
+      for j in range(self.max_j):
+        if board[i][j] == target and exists(board, word, set(), '', i, j, 0):
+          return True
+    return False
