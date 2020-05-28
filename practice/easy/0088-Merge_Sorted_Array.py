f67bf96646a9e99ba95e0c5feb90d61e33c82c52
Matt Peretick <mattjp@umich.edu>
2020-05-28 08:41:22 -0400

    Update 0088-Merge_Sorted_Array.py

--- practice/easy/0088-Merge_Sorted_Array.py
+++ practice/easy/0088-Merge_Sorted_Array.py
@@ -15,3 +15,20 @@ class Solution:
         nums1[k] = y
         j += 1
       k += 1
+      
+# solved again - iterating in reverse
+class Solution:
+  def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    p = m+n-1
+    m -= 1
+    n -= 1
+    while p >= 0:
+      x = nums1[m] if m >= 0 else -1000
+      y = nums2[n] if n >= 0 else -1000
+      if x >= y:
+        nums1[p] = x
+        m -= 1
+      else:
+        nums1[p] = y
+        n -= 1
+      p -= 1
