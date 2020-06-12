9a6c6e2a01312846953534d5878f9b7636b800ef
Matt Peretick <mattjp@umich.edu>
2020-06-12 16:03:28 -0400

    Update 0146-LRU_Cache.py

--- practice/medium/0146-LRU_Cache.py
+++ practice/medium/0146-LRU_Cache.py
@@ -63,3 +63,73 @@ class LRUCache:
     self.cache.move_to_end(key, last=False) # move cache[key] to the head of the dict
     if len(self.cache) > self.capacity:
       self.cache.popitem(last=True) # pop the least recently used item
+
+# solved the hard way - building out OrderedDict
+class dll_node:
+  def __init__(self, key=-1, val=-1):
+    self.key = key
+    self.val = val
+    self.next = None
+    self.prev = None
+
+
+class LRUCache:
+  def __init__(self, capacity: int):
+    self.cache = {} # int -> DLL node
+    self.capacity = capacity
+
+    # doubly-linked list
+    self.head = dll_node()
+    self.tail = dll_node()
+    self.head.next = self.tail
+    self.tail.prev = self.head
+        
+  def pop_tail(self):
+    p = self.tail.prev
+    p.prev.next = self.tail
+    self.tail.prev = p.prev
+    return p
+        
+  def move_to_head(self, key: int) -> None:
+    node = self.cache[key]
+
+    # if the node previously existed in the DLL -- delete it
+    if node.prev != None and node.next != None:
+      p = node.prev
+      n = node.next
+      p.next = n
+      n.prev = p
+        
+    # set the `next` pointer of the DLL head to the node
+    h = self.head.next    # old head
+    node.next = h         # new head next -> old head
+    h.prev = node         # old head previous -> new head
+    node.prev = self.head # new head prev -> DLL head
+    self.head.next = node # DLL head next -> new head
+
+  def get(self, key: int) -> int:
+    if key not in self.cache:
+      return -1
+    self.move_to_head(key)
+    return self.cache[key].val
+        
+  def put(self, key: int, value: int) -> None:
+    # create a new node if one does not exist
+    if key not in self.cache:
+      self.cache[key] = dll_node(key,value)
+    else:
+      self.cache[key].val = value
+
+    # update head of DLL
+    self.move_to_head(key)
+
+    # remove least-recently used
+    if len(self.cache) > self.capacity:
+      t = self.pop_tail()
+      del self.cache[t.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
