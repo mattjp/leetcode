c26732ac7501c7b461005cbe4a549d8cbd9ec02d
Matt Peretick <mattjp@umich.edu>
2020-06-13 20:49:50 -0400

    Update and rename practice/0011-Container_With_Most_Water.py to practice/medium/0011-Container_With_Most_Water.py

--- practice/medium/0011-Container_With_Most_Water.py
+++ practice/medium/0011-Container_With_Most_Water.py
@@ -20,3 +20,20 @@ class Solution:
       else:
         r -= 1
     return res
+
+# solved again
+class Solution:
+  def maxArea(self, height: List[int]) -> int: 
+    max_area = 0
+    l = 0
+    r = len(height)-1
+
+    while l < r:
+      max_area = max(max_area, min(height[l], height[r]) * (r - l))
+      if height[l] < height[r]:
+        l += 1
+      else:
+        r -= 1
+    return max_area
+
+  
