d9265e8b2420ca584468e606d4e80cc4f2f4908d
Matt Peretick <mattjp@umich.edu>
2020-08-04 08:46:14 -0400

    Update 0342-Power_of_Four.py

--- practice/easy/0342-Power_of_Four.py
+++ practice/easy/0342-Power_of_Four.py
@@ -7,3 +7,20 @@ class Solution:
       if x == num: return True
       x *= 4
     return False
+
+# no loops
+class Solution:
+  def isPowerOfFour(self, num: int) -> bool:
+
+    # 100
+    # 10000
+    # 1000000
+
+    b = bin(num)[2:]
+
+    w = num > 0
+    x = b[0] == '1'
+    y = len(b[1:]) % 2 == 0
+    z = int(b[1:]) == 0 if b[1:] else True
+
+    return True if w and x and y and z else False
