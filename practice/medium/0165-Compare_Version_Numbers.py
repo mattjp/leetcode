72d5661ee885f6d9594e8494e7ebf4bfcb20095a
Matt Peretick <mattjp@umich.edu>
2020-09-09 08:19:14 -0400

    Update 0165-Compare_Version_Numbers.py

--- practice/medium/0165-Compare_Version_Numbers.py
+++ practice/medium/0165-Compare_Version_Numbers.py
@@ -15,3 +15,17 @@ class Solution:
       elif ui < vi: return -1
 
     return 0
+  
+  
+# solved again
+# did the exact same thing lol
+class Solution:
+  def compareVersion(self, version1: str, version2: str) -> int:
+    v1 = list(map(int, version1.split('.')))
+    v2 = list(map(int, version2.split('.')))
+    while len(v1) < len(v2): v1.append(0)
+    while len(v1) > len(v2): v2.append(0)
+    for u,v in zip(v1, v2):
+      if u > v:   return 1
+      elif u < v: return -1
+    return 0
