a62ff6c9f79bf1b15017a05feab39c92210da749
Matt Peretick <mattjp@umich.edu>
2022-09-08 12:52:57 -0400

    Update 1094-Car_Pooling.py

--- practice/medium/1094-Car_Pooling.py
+++ practice/medium/1094-Car_Pooling.py
@@ -34,3 +34,48 @@ class Solution:
       heapq.heappush(inprogress, (end, start, passengers)) # this trip is now in progress
       
     return True
+  
+  
+# Solved again 9-9-22
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        """
+        Put all trips into a heap
+        Sort the heap with smallest start on top
+        Ties are broken with the smaller end
+        Pop from the top of the heap, update current capacity
+        Add the top to a heap with smallest end on top
+        If the next top of the start heap is greater than the end of the end heap
+            then pop from the end heap and decrement the current capacity
+        """
+        
+        # (start, end, num_passengers)
+        start_heap = list(map(lambda t: (t[1], t[2], t[0]), trips))
+        heapq.heapify(start_heap)
+        
+        # (end, start, num_passengers)
+        end_heap = []
+        
+        cur_cap = 0
+        
+        # Iterate through all trips
+        while start_heap:
+            top_start, top_end, top_num_pass = heapq.heappop(start_heap)
+            
+            # Pop all trips that have ended from the end heap
+            while end_heap and end_heap[0][0] <= top_start:
+                end, start, num_pass = heapq.heappop(end_heap)
+                cur_cap -= num_pass
+            
+            # Update car capacity and break if necessary
+            cur_cap += top_num_pass
+            if cur_cap > capacity:
+                return False
+            
+            # Add the new trip to the end heap
+            heapq.heappush(end_heap, (top_end, top_start, top_num_pass))
+
+        
+        # If all trips could be completed, we return True
+        return True
+
