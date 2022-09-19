51622b9722081787442fdc66cfbd48acab9ecff7
Matt Peretick <mattjp@umich.edu>
2022-09-18 20:09:56 -0400

    Update 0460-LFU_Cache.py

--- practice/hard/0460-LFU_Cache.py
+++ practice/hard/0460-LFU_Cache.py
@@ -56,3 +56,102 @@ class LFUCache:
 # obj = LFUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+
+# Solved again on 9/18
+from collections import OrderedDict
+
+class LFUCache:
+
+    def __init__(self, capacity: int):
+        """
+        Option 0.
+        Use a sorted dictionary 
+        
+        Option 1.
+        1. Map key -> frequency
+        2. Map frequency -> ordered dict of key-value pairs
+        3. Maintain min freq 
+        """
+        self.min_freq = 0
+        self.capacity = capacity
+        self.keys = {}
+        self.frequencies = {}
+        
+        
+    def addKey(self, key, value):
+        # Frequency will always start at 1
+        self.keys[key] = 1
+        
+        # If we're adding a key, it's always the least used key
+        self.min_freq = 1
+        
+        # Add key-value to end of ordered dict
+        if 1 not in self.frequencies:
+            self.frequencies[1] = OrderedDict()
+        self.frequencies[1][key] = value
+        
+        
+    def updateFrequency(self, key):
+        freq = self.keys[key]
+        
+        value = self.frequencies[freq][key]
+        
+        self.frequencies[freq].move_to_end(key)
+        
+        self.frequencies[freq].popitem()
+        
+        # If this was the last key at the min frequency, update min frequency
+        if len(self.frequencies[freq]) == 0 and self.min_freq == freq:
+            self.min_freq += 1
+            
+        self.keys[key] += 1
+        
+        freq += 1
+        
+        if freq not in self.frequencies:
+            self.frequencies[freq] = OrderedDict()
+            
+        self.frequencies[freq][key] = value
+
+        
+    def updateKey(self, key, value):
+        # Update the frequency of the key to be 1 higher
+        self.updateFrequency(key)
+        
+        # Update the key with the new value
+        freq = self.keys[key]
+        self.frequencies[freq][key] = value
+        
+        
+    def evict(self):
+        # Pop the least recently used key of the minimum frequency
+        key, value = self.frequencies[self.min_freq].popitem(last=False)
+        self.keys.pop(key)
+        
+        
+    def get(self, key: int) -> int:
+        if key not in self.keys:
+            return -1
+        freq = self.keys[key]
+        value = self.frequencies[freq][key]
+        self.updateFrequency(key)
+        
+        return value
+        
+        
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.keys:
+            self.updateKey(key, value)
+        else:
+            if len(self.keys) == self.capacity and self.capacity > 0:
+                self.evict()
+            if self.capacity > 0:
+                self.addKey(key, value)
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
