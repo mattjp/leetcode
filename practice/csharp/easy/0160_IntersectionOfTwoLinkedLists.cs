600410976c6c5f7d74dc0ae4c35b38b4622bfd12
Matt Peretick <mattjp@umich.edu>
2022-11-28 17:32:24 -0500

    Update 0160_IntersectionOfTwoLinkedLists.cs

--- practice/csharp/easy/0160_IntersectionOfTwoLinkedLists.cs
+++ practice/csharp/easy/0160_IntersectionOfTwoLinkedLists.cs
@@ -7,25 +7,25 @@
  * }
  */
 public class Solution {
-    public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
-      
-      // Create a set of nodes from list A
-      HashSet<ListNode> aNodes = new HashSet<ListNode>();
-      
-      ListNode pointer = headA;
-      
-      while (pointer != null) {
-        aNodes.Add(pointer);
-        pointer = pointer.next;
-      }
+  public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
 
-      // Iterate through list B until we find a node that exists in the set
-      pointer = headB;
-      
-      while (pointer != null && !aNodes.Contains(pointer)) {
-        pointer = pointer.next;
-      }
-      
-      return pointer;
+    // Create a set of nodes from list A
+    HashSet<ListNode> aNodes = new HashSet<ListNode>();
+
+    ListNode pointer = headA;
+
+    while (pointer != null) {
+      aNodes.Add(pointer);
+      pointer = pointer.next;
     }
+
+    // Iterate through list B until we find a node that exists in the set
+    pointer = headB;
+
+    while (pointer != null && !aNodes.Contains(pointer)) {
+      pointer = pointer.next;
+    }
+
+    return pointer;
+  }
 }
